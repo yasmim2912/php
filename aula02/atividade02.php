e9d057f4b37613f9eb4a1e1030178fec616550a4
yasmimrodrigues <145464519+yasmim2912@users.noreply.github.com>
2023-11-10 11:30:02 -0300

    Update atividade02.php

---
+++ aula02/atividade02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="atv02.css">
+</head>
+<body>
+    <div id="d01">titulo</div>
+    <div id="d02"></div>
+    <div id="d03"></div>
+    <div id="d04"></div>
+</body>
+</html>
