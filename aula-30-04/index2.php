171033c9faa5f39d9138399c3ce423f01e0b338a
yasmimrodrigues <145464519+yasmim2912@users.noreply.github.com>
2024-04-30 11:40:06 -0300

    index2.php

--- aula-30-04/index2.php
+++ aula-30-04/index2.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-    <?php
-
-echo"<br> <Ex:01></Ex:01><br>";
+echo"<P> <br> Ex:01<br> </P>";
 $var = array(1,2,3,4,5);
 echo "$var[0]<br>";
 echo "$var[1]<br>";
@@ -17,13 +8,13 @@ echo "$var[2]<br>";
 echo "$var[3]<br>";
 echo "$var[4]<br>";
 
-echo"<br> Ex:02 <br>";
+echo"<p> <br> Ex:02 <br> </p> ";
 $var1 =array("fabio"=>30,"eber"=>40 ,"wq"=>60 );
 echo "$var1[fabio]<br>";    
 echo  "$var1[eber]<br>";             
 echo  "$var1[wq]<br>";
 
-echo"<br> Ex:03 <br>";
+echo"<p> <br> Ex:03 <br> </p>";
 $var2 =array ("0"=>5,"6"=>8,"9"=>15);
 $var2 [1]=20; 
 echo  "$var2[0]<br>";
@@ -34,5 +25,6 @@ $var2 [0]=4;
 echo  "$var2[0]<br>";
 ?>
 
-</body>
+
 </html><link rel="stylesheet" href="index.css">
+
