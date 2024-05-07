53065e61d12367dda8258cf21e6e784f92c1d505
yasmimrodrigues <145464519+yasmim2912@users.noreply.github.com>
2024-05-07 08:58:43 -0300

    atividae01.php

--- aula-30-04/atividae01.php
+++ aula-30-04/atividae01.php
@@ -1,4 +1,56 @@
 <?php
-$aluno=array([nome])
 
-?>
+$aluno1["nome"] = "yasmim";
+$aluno1["idade"] = 18;
+$aluno1["endereço"] = "Minha casa";
+$aluno1["cidade"] = "juvenilia";
+$aluno1["estado"] = "minas gerais";
+$aluno1["cpf"] = "39467000";
+
+
+echo "Aluno 1: <br>";
+foreach ($aluno1 as $a) {
+    echo  key($aluno1) . ": $a  <br>";
+    next($aluno1);
+    }
+    
+$aluno2["nome"] = "Raifran";
+$aluno2["idade"] = 18;
+$aluno2["endereço"] = "";
+$aluno2["cidade"] = "Pesqueiro";
+$aluno2["estado"] = "Bahia";
+$aluno2["cpf"] = "457000";
+
+echo "<br>Aluno 2: <br>";
+foreach ($aluno2 as $c) {
+    echo  key($aluno2) . ": $c  <br>";
+    next($aluno2);
+}
+    
+$aluno3["nome"] = "isis";
+$aluno3["idade"] = 18;
+$aluno3["endereço"] = "casa";
+$aluno3["cidade"] = "pindai";
+$aluno3["estado"] = "Bahia";
+$aluno3["cpf"] = "709000";
+
+echo "<br> Aluno 3: <br>";
+echo "nome: $aluno3[nome]<br>";
+echo "idade: $aluno3[idade]<br>";
+echo "endereço: $aluno3[endereço]<br>";
+echo "cidade: $aluno3[cidade]<br>";
+echo "estado: $aluno3[estado]<br>";
+echo "cpf: $aluno3[cpf]<br>";
+    
+$professor["nome"] = "Fábio";
+$professor["idade"] = 30;
+$professor["endereço"] = "Um Lugar";
+$professor["cidade"] = "Guanambi";
+$professor["estado"] = "Bahia";
+$professor["cpf"] = "000000000";
+
+echo "<br>Professor: <br>";
+foreach ($professor as $d) {
+    echo  key($professor) . ": $d  <br>";
+    next($professor);
+}
