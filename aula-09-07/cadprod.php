d56e53eb8d8cde08e76284035da4d6b270ef2e37
yasmimrodrigues <145464519+yasmim2912@users.noreply.github.com>
2024-07-09 11:30:13 -0300

    Add files via upload

---
+++ aula-09-07/cadprod.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h2> cadastro de peroduto </h2>
+<label>
+    
+    <label for="name">produto:</label><br>
+    <input type="text" id="name" name="name" value=""> <br>
+    <label for="name"> marca:</label> <br>
+    <select id= "marca" name="marca" size= "4"multiple >
+        <option value="shein">shein</option>
+        <option value="zara">zara</option>
+        <option value="lovito">lovito</option>
+        <option value="CeA">CeA</option>
+    </select><br><br>
+    <label for="name">tamanho<>
+  <input type="submit">
+</form>
+</body>
+
+</html>
